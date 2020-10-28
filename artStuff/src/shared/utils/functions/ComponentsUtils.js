a47a63ca4c150fbe4545fc7f60c6380229114d16
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-28 12:31:22 -0300

    feat(utils): Create functions to detect orientation

--- artStuff/src/shared/utils/functions/ComponentsUtils.js
+++ artStuff/src/shared/utils/functions/ComponentsUtils.js
@@ -1,1 +1,7 @@
+import { Dimensions } from "react-native";
+
 export const hasSetFunctionProperty = (value) => value && typeof(value) === "function";
+
+export const isPortrait = () => Dimensions.get("screen").width >= Dimensions.get("screen").height;
+
+export const isLandscape = () => Dimensions.get("screen").width <= Dimensions.get("screen").height;
