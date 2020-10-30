5b308bda5b527afa276cb6b5fa8802e46ac1ef63
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-30 11:00:52 -0300

    feat(components): Create ToggleButtons

---
+++ artStuff/src/shared/components/ToggleButton/ToggleButton.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import {
+    StyledToggleButton,
+    StyledIcon
+} from "./styles";
+
+import { hasSetFunctionProperty } from "../../utils/functions/ComponentsUtils";
+
+const ToggleButton = ({
+    onEnable,
+    onDisable,
+    enableIcon,
+    disableIcon,
+    children
+}) => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const handleButtonPress = () => {
+        setIsActive(!isActive);
+
+        if (isActive && hasSetFunctionProperty(onEnable)) {
+            onEnable();
+        }
+        else if (!isActive && hasSetFunctionProperty(onDisable)) {
+            onDisable();
+        }
+    };
+
+    const getIcon = () => {
+        return isActive ? enableIcon : disableIcon;
+    };
+
+    console.log(children);
+
+    return (
+        <StyledToggleButton onPress={handleButtonPress}>
+            <StyledIcon source={getIcon()} />
+            {children}
+        </StyledToggleButton>
+    );
+};
+
+ToggleButton.propTypes = {
+    onEnable: PropTypes.func,
+    onDisable: PropTypes.func,
+    enableIcon: PropTypes.number.isRequired,
+    disableIcon: PropTypes.number.isRequired,
+};
+
+ToggleButton.defaultProps = {
+    onEnable: null,
+    onDisable: null,
+    enableIcon: null,
+    disableIcon: null,
+};
+
+export default ToggleButton;
---
+++ artStuff/src/shared/components/ToggleButton/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledToggleButton = styled.TouchableOpacity`
+    flex-direction: row;
+    padding: 3px 6px;
+`;
+
+export const StyledIcon = styled.Image`
+    height: 24px;
+    width: 24px;
+    margin-left: 6px;
+`;
