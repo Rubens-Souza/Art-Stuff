82ffd33e1dde7f86e63beec0f91cd8aae9b3a64b
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-30 13:29:07 -0300

    feat(features): Create Feed

--- artStuff/src/aplication/App.js
+++ artStuff/src/aplication/App.js
@@ -5,23 +5,15 @@ import {
 }from "./styles";
 
 import Header from "../features/Header/Header";
-import Post from "../shared/components/Post/Post";
-
-import Image from "../../assets/imgs/art-test.jpg";
-
-import UserData from "../shared/dtos/UserData";
-import PostData from "../shared/dtos/PostData";
+import Feed from "../features/Feed/Feed";
 
 const App = () => {
-
-  const fakePerson = new UserData("fake.person.job@gmail.com", "Fake Person");
-  const post = new PostData(fakePerson, "Flying", Image, []);
 
   return (
     <React.Fragment>
       <StyledSafeAreaView>
-        <Header />
-        <Post postData={post}/>
+          <Header />
+          <Feed />
       </StyledSafeAreaView>
     </React.Fragment>
   );
---
+++ artStuff/src/features/Feed/Feed.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+    StyledPostsList
+} from "./styles";
+
+import Post from "../../features/Post/Post";
+
+import KateFoxPostImage from "../../../assets/imgs/art-test.jpg";
+import YuumeiPostImage from "../../../assets/imgs/art-test4.jpg";
+import YuumeiPostImage2 from "../../../assets/imgs/art-test5.jpg";
+
+import UserData from "../../shared/dtos/UserData";
+import PostData from "../../shared/dtos/PostData";
+import CommentData from "../../shared/dtos/CommentData";
+
+const Feed = () => {
+
+    const kateFox = new UserData("fake.person.job@gmail.com", "Kate Fox");
+    const yuumei = new UserData("fake.person.job@gmail.com", "Yuumei");
+    
+    const katePost = new PostData(kateFox, "Flying", KateFoxPostImage, []);
+    const yuPost = new PostData(yuumei, "Flying", YuumeiPostImage, []);
+    const yuPost2 = new PostData(kateFox, "Flying", YuumeiPostImage2, []);
+
+    const comment = new CommentData(yuumei, "I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.");
+    let x = [comment];
+
+    const posts = [
+        katePost,
+        yuPost,
+        yuPost2,
+    ];
+
+    return (
+        <StyledPostsList
+            data={posts}
+            keyExtractor={post => post.id}
+            renderItem={({item: post}) => {
+              return <Post postData={post} />
+            }}
+        />
+    );
+};
+
+export default Feed;
