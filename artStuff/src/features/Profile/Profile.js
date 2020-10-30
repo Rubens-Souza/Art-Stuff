90cf0e6effd82d787d8ba9432d194f559ef72c97
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-30 17:16:27 -0300

    feat(features): Create base profile screen

---
+++ artStuff/src/features/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Icons from "../../shared/utils/constants/Icons";
+
+import ScreenData from "../../shared/dtos/ScreenData";
+
+const Profile = () => {
+    return null;
+};
+
+export const ProfileScreenData = new ScreenData(Profile.name, Icons.Liked, Icons.Like);
+export default Profile;
