ccd8eb3fb319b1373db3754ea524ae837c23c013
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-30 10:19:49 -0300

    style(post): Add horizontal line

--- artStuff/src/shared/components/Post/Post.js
+++ artStuff/src/shared/components/Post/Post.js
@@ -2,14 +2,16 @@ import React from "react";
 
 import {
     StyledPostView,
+    StyledHorizontalLineView,
+    StyledHorizontalLine,
     StyledPostImage
 } from "./styles";
 
 import Author from "../Author/Author";
 import Comment from "../Comment/Comment";
+import Comments from "../../../features/Comments/Comments";
 
 import CommentData from "../../dtos/CommentData";
-import userData from "../../dtos/UserData";
 import UserData from "../../dtos/UserData";
 
 const Post = ({
@@ -17,26 +19,21 @@ const Post = ({
 }) => {
 
     const user = new UserData("gamer@gmail.com", "Pro-Player");
-    const comment = new CommentData(user);
+    const comment = new CommentData(user, "I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.");
 
     return (
         <StyledPostView>
             <Author user={postData.author}>
                 {postData.title}
             </Author>
+
             <StyledPostImage source={postData.image}/>
-            <Comment commentData={comment}>
-            I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.
-            </Comment>
-            <Comment commentData={comment}>
-            I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.
-            </Comment>
-            <Comment commentData={comment}>
-            I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.
-            </Comment>
-            <Comment commentData={comment}>
-            I don't understand, you're saying this is digital??? If so, I'm impressed. Although I don't think I'll ever try it. I do digital work, and like how that comes out. But I also do traditional art, and don't want to mix the two.
-            </Comment>
+
+            <Comments/>
+
+            <StyledHorizontalLineView>
+                <StyledHorizontalLine/>
+            </StyledHorizontalLineView>
         </StyledPostView>
     );
 };
--- artStuff/src/shared/components/Post/styles.js
+++ artStuff/src/shared/components/Post/styles.js
@@ -2,17 +2,27 @@ import styled from "styled-components/native";
 import Colors from "../../utils/constants/Colors";
 
 export const StyledPostView = styled.View`
-    padding: 9px 0;
+    padding: 6px 0 9px 0;
+    margin-top: 6px;
     width: 100%;
     background-color: ${Colors.SnowWhite};
-    margin-bottom: 6px;
 `;
 
 export const StyledPostImage = styled.Image`
     width: 100%;
     height: auto;
     resize-mode: contain;
-    background-color: red;
     aspect-ratio: ${3/2};
     margin: 3px 0px;
+`;
+
+export const StyledHorizontalLineView = styled.View`
+    margin-top: 9px;
+    padding: 0 37%;
+`;
+
+export const StyledHorizontalLine = styled.View`
+    width: 100%;
+    border-bottom-width: 3px;
+    border-bottom-color: ${Colors.Black};
 `;
