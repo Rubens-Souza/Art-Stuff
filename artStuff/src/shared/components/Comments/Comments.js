5cbea9b8bb8a7a4be8e0f5af2d0ca6c67aa67e33
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-30 13:56:55 -0300

    feat(components): Create input to add comment

--- artStuff/src/shared/components/Comments/Comments.js
+++ artStuff/src/shared/components/Comments/Comments.js
@@ -3,21 +3,48 @@ import PropTypes from "prop-types";
 
 import {
     StyledCommentsView,
-    StyledCommentsList
+    StyledCommentsList,
+    StyledAddCommentView,
+    StyledAddCommentButton,
+    StyledAddCommentButtonIcon,
+    StyledCommentInput
 } from "./styles";
 
 import Comment from "../Comment/Comment";
 
+import Icons from "../../utils/constants/Icons";
+import { EmptyString } from "../../utils/functions/StringUtils";
+import { hasSetFunctionProperty } from "../../utils/functions/ComponentsUtils";
+
+import CommentData from "../../dtos/CommentData";
+
 const Comments = ({
-    commentsData
+    commentsData,
+    onNewCommentInsert,
 }) => {
 
-    const [comments] = useState(commentsData);
+    const [newComment, setNewComment] = useState(EmptyString);
+
+    const handleNewCommentChange = (text) => {
+        setNewComment(text);
+    };
+
+    const handleNewCommentInsert = () => {
+        let newCommentContent = newComment;
+        setNewComment(EmptyString);
+
+        // TODO: Add comment with user without getting the active user from props
+        // let newCommentData = new CommentData(activeUser, newCommentContent);
+
+        if (hasSetFunctionProperty(onNewCommentInsert)) {
+            onNewCommentInsert();
+        }
+    };
 
     return (
         <StyledCommentsView>
             <StyledCommentsList 
-                data={comments}
+                data={commentsData}
                 keyExtractor={comment => comment.id}
                 renderItem={({item: comment}) => {
                     return (
@@ -27,16 +54,29 @@ const Comments = ({
                     );
                 }}
             />
+            <StyledAddCommentView>
+                <StyledAddCommentButton onPress={handleNewCommentInsert}>
+                    <StyledAddCommentButtonIcon source={Icons.Plus} />
+                </StyledAddCommentButton>
+
+                <StyledCommentInput 
+                    placeholder={"Add a nice comment!"}
+                    value={newComment}
+                    onChange={handleNewCommentChange}
+                />
+            </StyledAddCommentView>
         </StyledCommentsView>
     );
 };
 
 Comments.propTypes = {
     commentsData: PropTypes.array.isRequired,
+    onNewCommentInsert: PropTypes.func,
 };
 
 Comments.defautProps = {
     commentsData: new Array(0),
+    onNewCommentInsert: null,
 };
 
 export default Comments;
--- artStuff/src/shared/components/Comments/styles.js
+++ artStuff/src/shared/components/Comments/styles.js
@@ -1,8 +1,34 @@
 import styled from "styled-components/native";
 
-export const StyledCommentsView = styled.View``;
+import Colors from "../../utils/constants/Colors";
+
+export const StyledCommentsView = styled.View`
+`;
 
 export const StyledCommentsList = styled.FlatList`
     padding: 9px 4px 6px 12px;
     width: 90%;
+`;
+
+export const StyledAddCommentView = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 3px 6px;
+`;
+
+export const StyledAddCommentButton = styled.TouchableOpacity`
+    margin-right: 6px;
+`;
+
+export const StyledAddCommentButtonIcon = styled.Image`
+    height: 24px;
+    width: 24px;
+`;
+
+export const StyledCommentInput = styled.TextInput`
+    width: 85%;
+    padding: 3px;
+    font-size: 15px;
+    border-bottom-color: ${Colors.Black};
+    border-bottom-width: 1px;
 `;
