e4531355fec6e61eb65e5c7599863f81ab717e88
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-11-03 15:15:00 -0300

    feat(features): Implements AddArt UI

--- artStuff/src/features/AddArt/AddArt.js
+++ artStuff/src/features/AddArt/AddArt.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
+    DefaultWhiteButtonText
+} from "../../shared/components/CommunStyles/CommunStyles";
 
+import {
+    StyledAddArtView,
+    StyledTextInput,
+    StyledWhiteButton,
+    StyledPostView
 } from "./styles";
 
 import ImagePicker from "../../shared/components/ImagePicker/ImagePicker";
 
 import Icons from "../../shared/utils/constants/Icons";
+import { EmptyString } from "../../shared/utils/functions/StringUtils";
 
 import ScreenData from "../../shared/dtos/ScreenData";
 
 const AddArt = () => {
 
-    
+    const [artTitle, setArtTitle] = useState(EmptyString);
+
+    const handleTextTitleChange = (text) => {
+        setArtTitle(text);
+    };
 
     return (
-        <ImagePicker/>
+        <StyledAddArtView>
+            <StyledPostView>
+                <StyledWhiteButton>
+                    <DefaultWhiteButtonText>
+                        Send
+                    </DefaultWhiteButtonText>
+                </StyledWhiteButton>
+
+                <StyledTextInput
+                    placeholder={"Add a title to your work of art"}
+                    value={artTitle}
+                    onChange={handleTextTitleChange}
+                />
+            </StyledPostView>
+            
+            <ImagePicker />
+        </StyledAddArtView>
     );
 };
 
