1852812b650e6ef4ddfb86050adadf5e8ba493cd
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-11-03 12:51:01 -0300

    feat(dto): Create image Dto

---
+++ artStuff/src/shared/dtos/ImageData.js
@@ -0,0 +1,12 @@
+class ImageData {
+
+    constructor(uri, content, height, width, formatType) {
+        this.uri = uri;
+        this.content = content;
+        this.height = height;
+        this.width = width;
+        this.formatType = formatType;
+    }
+}
+
+export default ImageData;
