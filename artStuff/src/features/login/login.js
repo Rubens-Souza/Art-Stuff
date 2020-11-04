ed281e92bb67eca538f2ac74d58dcd29f4932aa2
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-11-04 11:39:48 -0300

    feat(features): Create Login Screen

---
+++ artStuff/src/features/login/login.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+import {
+    StyledLoginSafeAreaView,
+    StyledTextInputView,
+    StyledTextInputLabel,
+    StyledTextInput,
+    StyledButtonsView,
+    StyledBlackButton,
+    StyledGhostBlackButton,
+    StyledBlackButtonText,
+    StyledGhostBlackButtonText,
+    StyledAppTitle,
+    StyledBackgroundImage,
+    StyledAppLogo,
+    StyledTitleView
+} from "./styles";
+
+import { HomeNavigatorScreenData } from "../HomeNavigator/HomeNavigator";
+
+import Icons from "../../shared/utils/constants/Icons";
+import ScreenData from "../../shared/dtos/ScreenData";
+
+import CoverPathLessTraveled from "../../../assets/imgs/Author-Yuumei-Path-Less-Traveled.jpg";
+
+const BackgroundCovers = [
+    CoverPathLessTraveled,
+];
+
+const Login = ({
+    navigation
+}) => {
+
+    const [emailInputValue, setEmailInputValue] = useState();
+    const [passwordInputValue, setPasswordInputValue] = useState();
+
+
+    const handleEmailChange = (text) => {
+        setEmailInputValue(text);
+    };
+
+    const handlePasswordChange = (text) => {
+        setPasswordInputValue(text);
+    };
+
+    const handleLogin = () => {
+        navigation.navigate(HomeNavigatorScreenData.name);
+    };
+
+    const handleCreateAccount = () => {
+
+    };
+
+    return (
+        <StyledBackgroundImage source={BackgroundCovers[0]}>
+            <StyledLoginSafeAreaView>
+
+                <StyledTitleView>
+                    <StyledAppLogo source={Icons.FavIcon} />
+
+                    <StyledAppTitle>
+                        Art Stuff
+                    </StyledAppTitle>
+                </StyledTitleView>
+
+                <StyledTextInputView>
+                    <StyledTextInputLabel>
+                        Email
+                    </StyledTextInputLabel>
+                    <StyledTextInput
+                        placeholder={"Type your email"}
+                        value={emailInputValue}
+                        onChange={handleEmailChange}
+                    />
+                </StyledTextInputView>
+
+                <StyledTextInputView>
+                    <StyledTextInputLabel>
+                        Password
+                    </StyledTextInputLabel>
+                    <StyledTextInput
+                        placeholder={"Type your password"}
+                        value={passwordInputValue}
+                        onChange={handlePasswordChange}
+                    />
+                </StyledTextInputView>
+
+                <StyledButtonsView>
+                    <StyledBlackButton>
+                        <StyledBlackButtonText onPress={handleLogin}>
+                            Login
+                        </StyledBlackButtonText>
+                    </StyledBlackButton>
+
+                    <StyledGhostBlackButton>
+                        <StyledGhostBlackButtonText onPress={handleCreateAccount}>
+                            Create Account
+                        </StyledGhostBlackButtonText>
+                    </StyledGhostBlackButton>
+                </StyledButtonsView>
+
+            </StyledLoginSafeAreaView>
+        </StyledBackgroundImage>
+    );
+};
+
+export const LoginScreenData = new ScreenData("Login");
+export default Login;
---
+++ artStuff/src/features/login/styles.js
@@ -0,0 +1,73 @@
+import styled from "styled-components/native";
+
+import {
+    DefaultStyledTitle,
+    DefaultStyledTextInput,
+    DefaultStyledText,
+    DefaultBlackButton,
+    DefaultBlackButtonText,
+    DefaultGhostBlackButton,
+    DefaultGhostButtonText,
+} from "../../shared/components/CommunStyles/CommunStyles";
+
+import Colors from "../../shared/utils/constants/Colors";
+
+export const StyledTitleView = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledAppLogo = styled.Image`
+    height: 72px;
+    width: 72px;
+    margin-right: 12px;
+    resize-mode: cover;
+`;
+
+export const StyledAppTitle = styled(DefaultStyledTitle)`
+    font-size: 51px;
+`;
+
+export const StyledBackgroundImage = styled.ImageBackground`
+    height: 100%;
+    width: 100%;
+`;
+
+export const StyledLoginSafeAreaView = styled.SafeAreaView`
+    height: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: ${Colors.SnowWhite + "ac"};
+`;
+
+export const StyledTextInputView = styled.View`
+    width: 81%;
+`;
+
+export const StyledTextInput = styled(DefaultStyledTextInput)`
+
+`;
+
+export const StyledTextInputLabel = styled(DefaultStyledText)`
+    font-size: 18px;
+`;
+
+export const StyledButtonsView = styled.View`
+    flex-direction: row;
+`;
+
+export const StyledBlackButton = styled(DefaultBlackButton)`
+    width: 30%;
+    margin-right: 6px;
+`;
+
+export const StyledBlackButtonText = styled(DefaultBlackButtonText)`
+`;
+
+export const StyledGhostBlackButton = styled(DefaultGhostBlackButton)`
+    width: 30%;
+`;
+
+export const StyledGhostBlackButtonText = styled(DefaultGhostButtonText)`
+`;
