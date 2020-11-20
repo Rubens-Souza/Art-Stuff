d320ddc42104b6c76718caee5956da824e0b9f8f
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-11-20 11:50:51 -0300

    feat(utils): Create regex patterns for validations

---
+++ artStuff/src/shared/utils/constants/RegexPatterns.js
@@ -0,0 +1,5 @@
+export const emailRegexPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gmi;
+
+export const passwordRegexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+export const specialCharactersExceptUnderscoreRegexPattern = /^[0-9a-zA-Z _.\b]+$/gmi;
